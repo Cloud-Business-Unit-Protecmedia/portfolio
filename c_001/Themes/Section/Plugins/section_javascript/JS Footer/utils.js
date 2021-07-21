96d925eba74f0f568152b3a4bc62ccbfdec7caa5
Dhimitri Pero <dpero@protecmedia.com>
2021-07-21 13:38:15 +0200

    Reorganizes the structure of theme c_001 in order to adapt to iterftd The theme is organized by subfolders named "Plugins" and "Themes" Inside the "Plugins" subfolder there are all the necessary structure of the plugins inside the same theme in Milenium. Inside the "Themes" subfolder there are the sub-themes of the theme in order for them to be applied where they belong. Each subtheme can have the same structure inside, divided by own plugins or other subthemes.

---
+++ c_001/Themes/Section/Plugins/section_javascript/JS Footer/utils.js
@@ -0,0 +1,34 @@
+﻿/*----utils----*/
+jQuery(".c_001_001.carrusel .noticias").bxSlider({
+        mode:'horizontal',
+            speed: 1000,
+            controls:true,
+            pager:false,
+            auto:false,
+            pause: 8000,
+            autoControls: false,
+            infiniteLoop: false,
+            hideControlOnEnd: true,
+            slideWidth: 375,
+            moveSlides:1,
+            minSlides:1,
+            maxSlides:40,
+            nextText: '',
+            prevText: ''
+    });
+
+function abrir_cerrar_masvideos(){
+	
+	if(jQuery(".c_001_001.noticias_masvideos2.column4").hasClass( "close" )){
+		jQuery(".c_001_001.noticias_masvideos2.column4").removeClass("close");
+		jQuery(".c_001_001.noticias_masvideos2.column4").addClass("open");
+	}
+	else{
+		jQuery(".c_001_001.noticias_masvideos2.column4").removeClass("open");
+		jQuery(".c_001_001.noticias_masvideos2.column4").addClass("close");
+	}
+
+
+ 
+
+}
